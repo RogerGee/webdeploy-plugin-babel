c0dabc44fe3a0213fe2f3587f5a93ee7992f7241
Roger Gee <rpg11a@acu.edu>
2020-08-15 16:29:48 -0500

    Implement babel plugin auditing

--- babel.js
+++ babel.js
@@ -1,27 +1,133 @@
 // babel.js - webdeploy build plugin
 
+const { format } = require("util");
 const babel = require("babel-core");
 
+async function loadBabelPackage(installer,type,map,plugin) {
+    var ref, options;
+    if (Array.isArray(plugin)) {
+        ref = plugin[0];
+        options = plugin[1];
+    }
+    else {
+        ref = plugin;
+        options = {};
+    }
+
+    // Leave plugin/preset untouched if it is not a string reference.
+    if (typeof ref !== 'string') {
+        return plugin;
+    }
+
+    var prefix = format("babel-%s-",type);
+
+    // Attempt to parse the plugin/preset reference string.
+    var match = ref.match(format("^(@[^/]+/)?(%s)?(.+)(@)(.+)$",prefix));
+    if (!match) {
+        throw new Error(format("Invalid babel plugin reference: %s",ref));
+    }
+
+    match[2] = prefix;
+    ref = match.slice(1).join("");
+
+    if (map.has(ref)) {
+        return m.get(ref);
+    }
+
+    var packageName = match.slice(1,4).join("");
+    var packageVersion = match[5];
+
+    var pluginModule = await new Promise((resolve,reject) => {
+        installer.installPackage(
+            packageName,
+            packageVersion,
+            (result) => {
+                try {
+                    resolve(installer.require());
+                } catch (err) {
+                    reject(err);
+                }
+            },
+            () => {
+                reject(
+                    new Error(format("Failed to install Babel package '%s'",packageName))
+                );
+            },
+            reject
+        );
+    });
+
+    map.set(ref,pluginModule);
+
+    return pluginModule;
+}
+
 module.exports = {
-    exec: (target,settings) => {
+    exec: async (target,settings) => {
         // Normalize settings.
         settings.presets = settings.presets || [];
         settings.plugins = settings.plugins || [];
 
-        return new Promise((resolve,reject) => {
-            target.loadContent().then((code) => {
-                var options = {
-                    presets: settings.presets,
-                    plugins: settings.plugins
-                };
+        let code = await target.loadContent();
+        let babelOptions = {
+            presets: settings.presets,
+            plugins: settings.plugins
+        };
 
-                var transpilation = babel.transform(code,options);
-                var outputTarget = target.makeOutputTarget();
+        let transpilation = babel.transform(code,babelOptions);
+        let outputTarget = target.makeOutputTarget();
 
-                outputTarget.stream.end(transpilation.code);
-                resolve(outputTarget);
+        outputTarget.stream.end(transpilation.code);
 
-            }).catch(reject);
+        return outputTarget;
+    },
+
+    audit: async (context,settingsList) => {
+        let plugins = new Map();
+        let presets = new Map();
+
+        let installer = new context.package.NPMPackageInstaller({
+            installPath: await context.makeCachePath("modules"),
+            logger: context.logger,
+            overwrite: false,
+
+            // Disallow running scripts on Babel plugins when they are
+            // installed.
+            noscripts: true,
+
+            // TODO Allow registries to be configured.
+            npmRegistries: [
+                'https://registry.npmjs.org/'
+            ]
         });
+
+        // Load plugins and presets. This replaces each string reference with a
+        // loaded node module corresponding to the downloaded babel
+        // plugin/preset package.
+        for (let i = 0;i < settingsList.length;++i) {
+            let settings = settingsList[i];
+
+            if (settings.plugins) {
+                for (let i = 0;i < settings.plugins.length;++i) {
+                    settings.plugins[i] = await loadBabelPackage(
+                        installer,
+                        'plugin',
+                        plugins,
+                        settings.plugins[i]
+                    );
+                }
+            }
+
+            if (settings.presets) {
+                for (let i = 0;i < settings.presets.length;++i) {
+                    settings.presets[i] = await loadBabelPackage(
+                        installer,
+                        'preset',
+                        presets,
+                        settings.presets[i]
+                    );
+                }
+            }
+        }
     }
 };
